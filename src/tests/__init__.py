c6be7b45bb8a5cc3a0b02239ffaa1867352f7a71
jazzu72 <lesane1972@gmail.com>
2026-01-04 08:37:32 -0500

    Update __init__.py

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,14 +1,7 @@
-src/tests/__init__.py"""
-Nia LeSane Quantum Tests Package
-Test suite for quantum backend integration
 """
-
-__version__ = "1.0.0"
-__author__ = "jazzu72""""
 Nia LeSane Quantum Tests Package
 Test suite for quantum backend integration
 """
 
 __version__ = "1.0.0"
 __author__ = "jazzu72"
-
