5a29eb587d53eb5e8e483cf0222dbbfa51684811
jazzu72 <lesane1972@gmail.com>
2026-03-13 10:49:17 -0400

    Create pressure.ts

---
+++ pressure.ts
@@ -0,0 +1,9 @@
+export function pressureWrapper(content: string): string {
+  return `
+<b>Nia (Pressure Mode)</b><br>
+Take a breath. The situation feels heavier than it is.<br>
+Here’s the truth beneath the noise:<br><br>
+${content}<br><br>
+We stay focused. We stay sharp. We win.
+  `;
+}
